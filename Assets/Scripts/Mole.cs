82e7f61be1d5917ce78634de20931e48e35beb7c
EricRFranco <france2@rpi.edu>
2016-10-09 12:56:41 -0400

    Add files via upload

---
+++ Assets/Scripts/Mole.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mole : MonoBehaviour {
+
+    public float offset;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter2D (Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            //player loses
+        }
+    }
+
+   void Rise ()
+    {
+        transform.Translate(0f, offset, 0f);
+    }
+
+    void Burrow ()
+    {
+        transform.Translate(0f, -offset, 0f);
+    }
+}
